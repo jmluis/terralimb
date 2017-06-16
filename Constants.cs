e740d19fb5825f97c0bc6293d6a2c2cc260234af
Jose Luis <shoot@chbshoot.me>
2017-06-16 17:36:21 -0400

    Package
    Terraria information as well as relevant data stored in Package struct
    
    Loaded from Json

--- Constants.cs
+++ Constants.cs
@@ -3,18 +3,27 @@ using Newtonsoft.Json;
 
 namespace TerraLimb
 {
+    public struct Package
+    {
+        public string Version;
+        public int TerrariaVersion;
+        public string TerrariaVersionString;
+        public int ProjectNo;
+        public int RevNo;
+    }
+
     public static class Constants
     {
-        public const int TERRARIA_RELEASE = 0xBC; // curRelease = 188
-        public const string TERRARIA_STRING = "1.3.4.4";
+        public static Package CurrentPackage;
         public static bool Loaded = false;
 
         public static Dictionary<int, Item> Items;
         public static Dictionary<int, Prefix> Prefixes;
         public static Dictionary<int, Buff> Buffs;
 
-        public static void Initialize(string ItemJson, string PrefixJson, string BuffJson)
+        public static void Initialize(string PackageJson, string ItemJson, string PrefixJson, string BuffJson)
         {
+            CurrentPackage = JsonConvert.DeserializeObject<Package>(PackageJson);
             Items = JsonConvert.DeserializeObject<Dictionary<int, Item>>(ItemJson);
             Prefixes = JsonConvert.DeserializeObject<Dictionary<int, Prefix>>(PrefixJson);
             Buffs = JsonConvert.DeserializeObject<Dictionary<int, Buff>>(BuffJson);
